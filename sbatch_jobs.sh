35b073d16f968a64feffa4f54e0e302a676a9822
Mingxuan Mei <s1871044@sms.ed.ac.uk>
2019-03-14 13:10:50 +0000

    mini experiments

---
+++ sbatch_jobs.sh
@@ -0,0 +1,11 @@
+scontrol show jobid -dd 301465 #poke_evol_mini_px2px_standard_single_gpu full_data
+
+scontrol show jobid -dd 301466 #poke_evol_mini_reduced_px2px_standard_single_gpu reduced_data
+
+# killed scontrol show jobid -dd 301480 #poke_evol_mini_px2px_short_single_gpu full_data
+
+# killed scontrol show jobid -dd 301482 #poke_evol_mini_reduced_px2px_standard_short_gpu reduced_data
+
+scontrol show jobid -dd 301572 #poke_evol_mini_px2px_short_single_gpu full_data 3:59:00 timelimit
+
+scontrol show jobid -dd 301573 #poke_evol_mini_reduced_px2px_standard_short_gpu reduced_data 3:59:00 timelimit
