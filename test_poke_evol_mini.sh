5d4f590077c958cf804329e725fdd3c3ffb9f278
Mingxuan Mei <s1871044@sms.ed.ac.uk>
2019-03-21 20:50:22 +0000

    130 test samples

--- test_poke_evol_mini.sh
+++ test_poke_evol_mini.sh
@@ -1,38 +1,7 @@
 #!/bin/sh
-#SBATCH -N 1	  # nodes requested
-#SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Short
-#SBATCH --gres=gpu:1
-#SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-03:00:00
 
-export CUDA_HOME=/opt/cuda-9.0.176.1/
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --gpu_ids -1
 
-export CUDNN_HOME=/opt/cuDNN-7.0/
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 50 --gpu_ids -1
 
-export STUDENT_ID=$(whoami)
-
-export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
-
-export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
-
-export CPATH=${CUDNN_HOME}/include:$CPATH
-
-export PATH=${CUDA_HOME}/bin:${PATH}
-
-export PYTHON_PATH=$PATH
-
-mkdir -p /disk/scratch/${STUDENT_ID}
-
-
-export TMPDIR=/disk/scratch/${STUDENT_ID}/
-export TMP=/disk/scratch/${STUDENT_ID}/
-
-mkdir -p ${TMP}/datasets/
-export DATASET_DIR=${TMP}/datasets/
-# Activate the relevant virtual environment:
-
-
-source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-#python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA  --display_id 0 #--gpu_ids -1
-python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --epoch 50 #--gpu_ids -1
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 55 --gpu_ids -1
