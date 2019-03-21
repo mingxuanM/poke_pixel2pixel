7b7c40fbf04fc5308ad68881f700b18933dd4128
Mingxuan Mei <s1871044@sms.ed.ac.uk>
2019-03-21 22:06:15 +0000

    full_body test

---
+++ test_poke_evol_full_body.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python test.py --dataroot ./datasets/poke_evol --name poke_evol_pix2pix_cleaned --model pix2pix --direction BtoA  --num_test 130 --gpu_ids -1 #--epoch 50 #--gpu_ids -1
+#python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 #--epoch 50 #--gpu_ids -1
+
+#python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 50 #--gpu_ids -1
+
+#python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 55 #--gpu_ids -1
