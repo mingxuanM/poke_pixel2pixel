a86d202f35b67e7395c47a6f9ac85ab7df8a4030
Mingxuan Mei <s1871044@sms.ed.ac.uk>
2019-03-22 11:53:13 +0000

    test aug 30 70 90

--- test_poke_evol_mini.sh
+++ test_poke_evol_mini.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --gpu_ids -1
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 30 --gpu_ids -1
 
-python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 50 --gpu_ids -1
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 70 --gpu_ids -1
 
-python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 55 --gpu_ids -1
+python test.py --dataroot ./datasets/evol_pairs_mini_rgb --name poke_evol_mini_reduced_aug --model pix2pix --direction BtoA  --num_test 130 --epoch 90 --gpu_ids -1
